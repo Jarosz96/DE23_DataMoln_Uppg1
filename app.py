cd7ce484400f4b0eae94dadab0f83a919aa79fc8
Sebbe <sebastian.jarosz96@gmail.com>
2024-09-28 14:36:53 +0200

    revert

--- app.py
+++ app.py
@@ -1,58 +1,36 @@
 from flask import Flask, render_template
 import pyodbc
-import plotly.express as px
-import plotly.io as pio
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 app = Flask(__name__)
 
-# Database connection setup
-def get_db_connection():
-    server = os.getenv('SERVER')
-    database = os.getenv('DATABASE')
-    username = os.getenv('DB_USERNAME')
-    password = os.getenv('PASSWORD')
-    driver = os.getenv('DRIVER')
-    
-    conn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+# Connectrion details for SQL server
+load_dotenv()
+server = os.getenv('SERVER')
+database = os.getenv('DATABASE')
+username = os.getenv('DB_USERNAME')
+password = os.getenv('PASSWORD')
+driver = os.getenv('DRIVER')
 
-    return conn
+# Database connection
+conn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+cursor = conn.cursor()
 
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    
-    query = '''
-    SELECT year, SUM(CASE WHEN gender_code = 1 THEN total_population ELSE 0 END) AS male_population,
-           SUM(CASE WHEN gender_code = 2 THEN total_population ELSE 0 END) AS female_population
-    FROM dbo.population_data
-    GROUP BY year
-    ORDER BY year
-    '''
-    
-    cursor.execute(query)
+    cursor.execute("SELECT year, SUM(CASE WHEN gender_code = 1 THEN total_population ELSE 0 END) AS male_population, "
+                   "SUM(CASE WHEN gender_code = 2 THEN total_population ELSE 0 END) AS female_population "
+                   "FROM dbo.population_data "
+                   "GROUP BY year "
+                   "ORDER BY year")
     data = cursor.fetchall()
-    conn.close()
 
-    # Prepare data for the graph
     years = [row[0] for row in data]
     male_population = [row[1] for row in data]
     female_population = [row[2] for row in data]
 
-    # Create Plotly figure
-    fig = px.line(x=years, y=[male_population, female_population], labels={'x': 'Year', 'y': 'Population'},
-                  title="Population by Gender Over Time", 
-                  color_discrete_map={"Male Population": 'blue', "Female Population": 'red'})
-    
-    fig.update_layout(xaxis_title='Year', yaxis_title='Population')
-
-    # Render the graph as an HTML component
-    graph_html = pio.to_html(fig, full_html=False)
-    
-    return render_template('index.html', graph_html=graph_html, zip=zip)
+    return render_template('index.html', years=years, male_population=male_population, female_population=female_population, zip=zip)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
